ef6f3af54a134404d2cbe9b2ee55ad169c06c8ae
Johan Lundahl <johan.t.lundahl@gmail.com>
2018-10-11 18:57:48 +0200

    Added creation of chart

--- week_summary.py
+++ week_summary.py
@@ -22,23 +22,27 @@ week_washes = []
 days = []
 for day in weekdays:
     washes, code = stats.get_washes_for(day)
-    week_washes += washes
     days.append(washes)
 
+total = sum(list(map(len, days)))
+    
 unknown = filter_count(days, unknown_check)
 commented = filter_count(days, commented_check)
 returned = filter_count(days, returned_check)
 
-#x_axis = list(map(lambda x: x.short_name(), weekdays))
 
-#chart1 = chart.StackedBar(x_axis)
-#chart1.add_bar(unknown, 'okända')
-#chart1.add_bar(commented, 'kommenterade')
-#chart1.add_bar(returned, 'levererade')
-#chart1.save('week.png')
-    
-success_rate = percent(sum(returned)/len(week_washes))
-comment_rate = percent(sum(commented)/len(week_washes))
+x_axis = list(map(lambda x: x.short_name(), weekdays))
+
+chart1 = chart.StackedBar(x_axis)
+chart1.add_bar(unknown, 'okända')
+chart1.add_bar(commented, 'kommenterade')
+chart1.add_bar(returned, 'levererade')
+chart1.save('charts/{}_{}.png'.format(week._of_year, week._number))
+
+url = 'http://31.208.165.29:5010/charts/year/{}/week/{}'.format(week._of_year, week._number)
+
+success_rate = percent(sum(returned)/total)
+comment_rate = percent(sum(commented)/total)
 message = 'Vecka {} tvättades {}% av bokningarna. {}% med kommentar. '.format(week._number, success_rate, comment_rate)
 
 stats.post_to_slack(message)
